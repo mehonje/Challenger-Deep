8e028536d76021b566cb406b6a7db37cf3cb5245
mehonje <168048262+mehonje@users.noreply.github.com>
2025-07-09 11:10:02 -0500

    Update sprite.py

--- challengerDeep/sprite.py
+++ challengerDeep/sprite.py
@@ -18,25 +18,20 @@ class Sprite:
             self.costumePath = pygame.image.load("costumes/costume1.png").convert_alpha()
 
     def stamp(self, transparency, size):
-        #direction
         self.costume = pygame.transform.rotate(self.costumePath, -self.dir)
         self.rect = self.costume.get_rect()
-        #size
-        self.costume = pygame.transform.scale(self.costume, (self.rect.width * (size * 0.01), self.rect.height * (size * 0.01)))
-        self.rect.x, self.rect.y = convertCoords(self.x, self.y, self.rect)
-        #transparency
+        self.costume = pygame.transform.scale(self.costume, (self.rect.width * (size * (self.gameEngine.width / 480) * 0.01), self.rect.height * (size * (self.gameEngine.height / 360) * 0.01)))
+        self.rect = self.costume.get_rect()
+        self.rect.x, self.rect.y = self.convertCoords(self.x, self.y, self.rect)
         self.costume.set_alpha(255 * (1 - transparency / 100))
         self.screen.blit(self.costume, self.rect)
 
-def convertCoords(x, y, rect, screenWidth = 480, screenHeight = 360):
-    match rect:
-        case "":
-            x = (screenWidth / 2) + x
-            y = (screenHeight / 2) - y
-        case _:
-            x = ((screenWidth / 2) + x) - (rect.width / 2)
-            y = ((screenHeight / 2) - y) - (rect.height / 2)
-    try:
+    def convertCoords(self, x, y, rect):
+        match rect:
+            case "":
+                x = (self.gameEngine.width / 2) + (x * (self.gameEngine.width / 480))
+                y = (self.gameEngine.height / 2) - (y * (self.gameEngine.height / 360))
+            case _:
+                x = ((self.gameEngine.width / 2) + (x * (self.gameEngine.width / 480))) - (rect.width / 2)
+                y = ((self.gameEngine.height / 2) - (y * (self.gameEngine.height / 360))) - (rect.height / 2)
         return int(x), int(y)
-    except ValueError:
-        return 0, 0
